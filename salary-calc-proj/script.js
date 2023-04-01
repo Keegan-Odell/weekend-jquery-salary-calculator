8bf1f097b8d788030301df46d6f4bf6f73679e3a
Keegan Odell <Keegan.Odell@icloud.com>
2023-04-01 16:34:19 -0500

    submit button grabs values

--- salary-calc-proj/script.js
+++ salary-calc-proj/script.js
@@ -6,8 +6,24 @@ function onReady() {
   $('#submit-button').on('click', onSubmit);
 }
 
+//This will handle the submit button
+//It will take the values fromt he inputs
+//then write them into the table
 function onSubmit() {
+  //prevents submit button from reloading page
   event.preventDefault();
+
+  //these variables are the values of the inputs
   let firstName = $('#first-name').val();
+  let lastName = $('#last-name').val();
+  let id = $('#id').val();
+  let title = $('#title').val();
+  let annualSalary = $('#annual-salary').val();
+
+  //testing these variables with console.logs
   console.log(firstName);
+  console.log(lastName);
+  console.log(id);
+  console.log(title);
+  console.log(annualSalary);
 }
